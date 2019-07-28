d58432c2ed9c0a3c57f168b2574a5a301325b19b
Thiago Costa <thiguet2014@gmail.com>
2019-07-27 22:41:32 -0300

    Using gh-pages branch to run this app.

---
+++ precache-manifest.1ee9734f32d74a563e9061fafdae3eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "829adfee6035df4dd353",
+    "url": "/static/js/main.829adfee.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "829adfee6035df4dd353",
+    "url": "/static/css/main.7d5c98cd.chunk.css"
+  },
+  {
+    "revision": "7e4288fd57e20801c6436e7c63982465",
+    "url": "/index.html"
+  }
+];
